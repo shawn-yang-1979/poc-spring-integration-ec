5f0e38431a116719ece925a3e405dccb6c5d8dde
htc <shawn_yang@htc.com>
2016-03-16 15:54:09 +0800

    First commit

---
+++ demo-spring-integration/src/main/java/com/shawnyang/poc/spring/integration/DemoSpringIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.shawnyang.poc.spring.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringIntegrationApplication.class, args);
+	}
+}
---
+++ demo-spring-integration/src/test/java/com/shawnyang/poc/spring/integration/DemoSpringIntegrationApplicationTests.java
@@ -0,0 +1,18 @@
+package com.shawnyang.poc.spring.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.shawnyang.poc.spring.integration.DemoSpringIntegrationApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemoSpringIntegrationApplication.class)
+public class DemoSpringIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
