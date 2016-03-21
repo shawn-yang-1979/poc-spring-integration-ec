600764602a88286bc6d39c0df6897ef6941a982b
htc <shawn_yang@htc.com>
2016-03-21 20:23:18 +0800

    rename

---
+++ poc-spring-integration-esb/src/test/java/com/shawnyang/poc/spring/integration/DemoSpringIntegrationApplicationTests.java
@@ -0,0 +1,18 @@
+package com.shawnyang.poc.spring.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.shawnyang.poc.spring.integration.BootApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BootApplication.class)
+public class DemoSpringIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
